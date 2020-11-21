7427d12c543fcc66d56c5b7a82759777b27011e7
lo <lo.sansa@outlook.com>
2020-11-21 18:10:30 +0000

    DP-1 Добавлены yahoo, arima

---
+++ pages/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from . import views
+
+
+
+
+# urlpatterns = [
+#     path('',views.my_first_view, name=''),
+#     path('',views.my_second_view, name=''),
+#     path('test_form/',views.test_form,name='test_form'),
+#     path('contact/',views.contact, name='contact'),
+#     path('graph/',views.graph, name='graph'),
+# ]

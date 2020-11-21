80e2b87bf8dca21345dfdf77c250ad44d9ed925e
lo <lo.sansa@outlook.com>
2020-11-21 16:07:24 +0000

    ASDA-5 bp

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'diplomproject',
         'USER': 'lo',
-        'PASSWORD': '123',
+        'PASSWORD': '1231',
         'HOST': 'localhost',
         'PORT': '',
     }

e420bb050ac449156390d54ea09e0cd7ad5571ec
lo <lo.sansa@outlook.com>
2020-11-23 18:32:06 +0000

    DP-7 Переименование столбцов таблицы

--- trade/models.py
+++ trade/models.py
@@ -21,7 +21,17 @@ class Quote(models.Model):
  
 import django_tables2 as tables
 class QuoteTable(tables.Table):
-    class Meta:
-        model = Quote
-        attrs = {'class': 'table table-hover table-sm'
-        }
+  def __init__(self, *args, c0="№", c1="Дата/Время",c2="Цена открытия",c3="Самая высокая цена",c4="Самая низкая цена",c5="Цена закрытия",c6="Ср. цена закрытия",c7="Объем",c8="Наименование",**kwargs):
+        super().__init__(*args, **kwargs)
+        self.base_columns['date_time'].verbose_name = c1
+        self.base_columns['q_open'].verbose_name = c2
+        self.base_columns['q_high'].verbose_name = c3
+        self.base_columns['q_low'].verbose_name = c4
+        self.base_columns['q_close'].verbose_name = c5
+        self.base_columns['q_adj_close'].verbose_name = c6
+        self.base_columns['q_volume'].verbose_name = c7
+        self.base_columns['q_name'].verbose_name = c8
+  class Meta:
+      model = Quote
+      attrs = {'class': 'table table-hover table-sm'
+      }
